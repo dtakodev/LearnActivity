f857910ef9719802dd0aa81c6da2bf0078d8839f
trung1999hy <trantrung1999hy@gmail.com>
2022-02-12 23:27:54 +0700

    .

---
+++ app/src/main/java/com/kovappkoi/learnactivity/adapter/SongAdapter.kt
@@ -0,0 +1,22 @@
+package com.kovappkoi.learnactivity.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.kovappkoi.learnactivity.databinding.ItemSongBinding
+
+class SongAdapter : RecyclerView.Adapter<SongAdapter.SongViewHolder>() {
+    class SongViewHolder(binding: ItemSongBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SongViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
